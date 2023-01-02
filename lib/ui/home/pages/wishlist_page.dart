bb25fbcfdb2fe168aa651c5cba7906ffa69ffa74
Samandar <samandarahadjonov@gmail.com>
2023-01-02 14:44:29 +0500

    creating architecture started

---
+++ lib/ui/home/pages/wishlist_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class WishListPage extends StatefulWidget {
+  const WishListPage({Key? key}) : super(key: key);
+
+  @override
+  State<WishListPage> createState() => _WishListPageState();
+}
+
+class _WishListPageState extends State<WishListPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
