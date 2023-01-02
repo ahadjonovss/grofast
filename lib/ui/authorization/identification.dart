991c428b1868ed79482b405e60a2bd8f459be0ca
Samandar <samandarahadjonov@gmail.com>
2023-01-02 14:37:55 +0500

    creating architecture started

---
+++ lib/ui/authorization/identification.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class IdentificationPage extends StatefulWidget {
+  const IdentificationPage({Key? key}) : super(key: key);
+
+  @override
+  State<IdentificationPage> createState() => _IdentificationPageState();
+}
+
+class _IdentificationPageState extends State<IdentificationPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
