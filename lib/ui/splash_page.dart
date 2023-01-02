03b19dbcb0d28cd319e7c88bc73d3730645db211
Samandar <samandarahadjonov@gmail.com>
2023-01-02 18:43:54 +0500

    splash page done

--- lib/ui/splash_page.dart
+++ lib/ui/splash_page.dart
@@ -1,10 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:grofast/utils/contants/image.dart';
+import 'package:grofast/utils/routes/app_routes.dart';
 
 class SplashPage extends StatelessWidget {
   const SplashPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    Future.delayed(const Duration(seconds: 3)).then((value) =>
+        Navigator.pushNamedAndRemoveUntil(
+            context, RouteName.onboardingPage, (route) => false));
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          decoration: const BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage(AppImages.splash), fit: BoxFit.fill)),
+        ),
+      ),
+    );
   }
 }
